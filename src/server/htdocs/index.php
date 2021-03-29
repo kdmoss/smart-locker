81182efac110181ffaff2407fd6728a45a4f710c
Kaid Mossburgh <kdmossbu@ksu.edu>
2021-03-29 11:50:42 -0500

    Add MySQL data

--- src/server/htdocs/index.php
+++ src/server/htdocs/index.php
@@ -2,6 +2,27 @@
 	require_once '../vendor/autoload.php';
 
 	// Verify that Twig has loaded
-	$loader = new \Twig\Loader\FilesystemLoader();
-	echo "Hello, World!";
+
+	function Database()
+	{
+		$db_host = "localhost";
+		$db_user = "root";
+		$db_pass = "";
+		$db_name = "smart_lock";
+
+
+		$db = new mysqli($db_host, $db_user, $db_pass);
+
+		if ($db->connect_errno)
+			die("Connection failed: %s\n" . $db->connect_errno);
+
+		return $db;
+	}
+
+	$db = Database();
+
+	$active = $db->query("SELECT * FROM smart_lock.test");
+	var_dump($active->fetch_assoc());
+
+	$db->close();
 ?>
