02d1c4c7777dc59c2c667f12149181674c55261e
Kaid Mossburgh <kdmossbu@ksu.edu>
2021-04-26 12:29:14 -0500

    Update lock.ino

--- src/lock/lock.ino
+++ src/lock/lock.ino
@@ -1,11 +1,12 @@
 /* Reference: */ 
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-// #include <SPI.h>
-// #include <WiFiNINA.h>
+
+extern "C" {
+  #include "user_interface.h"
+  #include "wpa2_enterprise.h"
+}
 
 // WLAN configuration
 const char *ssid     = "KSU Wireless";
@@ -62,10 +63,36 @@ void initOutput()
   digitalWrite(solenoidPin, 0);
 }
 
+void clearWLAN()
+{
+  wifi_station_clear_cert_key();
+  wifi_station_clear_enterprise_ca_cert();
+  wifi_station_clear_enterprise_identity();
+  wifi_station_clear_enterprise_username();
+  wifi_station_clear_enterprise_password();
+  wifi_station_clear_enterprise_new_password();
+}
+
 // Init wlan connection
 void initWLAN()
 {
-  WiFi.beginEnterprise(ssid, username, password);
+  WiFi.mode(WIFI_SATA);
+  wifi_set_opmode(STATION_MODE);
+  
+  struct station_config wifi_config;
+  memset(&wifi_config, 0, sizeof(wifi_config));
+  strcpy((char*)wifi_config.ssid, ssid);
+
+  clearWLAN();
+  
+  wifi_station_set_config(&wifi_config);
+  wifi_station_set_wpa2_enterprise_auth(1);
+
+  wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
+  wifi_station_connect();
+  
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -74,11 +101,11 @@ void initWLAN()
 
 void setup(void)
 {
-  Serial.begin ( 115200 );
+  Serial.begin(115200);
   initOutput();
   initWLAN();
   Serial.print("Connected to ");
-  Serial.println(ssid );
+  Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
