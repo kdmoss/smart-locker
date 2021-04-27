7e4ca874fb5574606219b5524ee4fdfcf093946a
Kaid Mossburgh <kdmossbu@ksu.edu>
2021-04-27 10:51:13 -0500

    Remove unnecessary extern

--- src/lock/lock.ino
+++ src/lock/lock.ino
@@ -2,12 +2,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-extern "C" 
-{
-  #include "user_interface.h"
-  #include "wpa2_enterprise.h"
-}
-
 ESP8266WebServer server(80);
 
 // WLAN configuration
